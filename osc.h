5ba97e1602446fd5a4d3252ca9f7f7d772f33b04
Ingo Fründ <mail@ingofruend.net>
2015-10-04 22:38:29 +0200

    quit handler

--- osc.h
+++ osc.h
@@ -47,10 +47,13 @@ int disconnect_handler(const char *path, const char *types, lo_arg ** argv,
                 int argc, void *data, void *user_data);
 int reconnect_handler(const char *path, const char *types, lo_arg ** argv,
                 int argc, void *data, void *user_data);
+int quit_handler(const char *path, const char *types, lo_arg ** argv,
+                int argc, void *data, void *user_data);
 
 int osc_start(struct deck *deck, struct library *library, size_t ndeck);
 void osc_stop();
 
 int osc_send_status(lo_address a, int d);
+
 
 #endif
